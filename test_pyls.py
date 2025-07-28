cc93291390f489fed0b8941297fd6b678d60cbf6
Apurva Varne <apurvavarne@gmail.com>
2025-07-28 11:09:09 +0000

    this is another test

--- test_pyls.py
+++ test_pyls.py
@@ -6,23 +6,36 @@ import pytest
 
 # Import the module whose procedures and functions you want to test here.
 import pyls
+import io
+import sys
 
 # Note that the procedures below that are testing for things **must**
 # have their names start with "test_" ... just as this file's name
 # also starts with "test_". `pytest` will automatically pick up
 # such functions and run them when you run `uvx pytest` from the
 # command line.
-def test_dummy():
+
+
+@pytest.fixture()
+def capture():
+    sio = io.StringIO()
+    yield sio
+def test_dummy(): #by convention, for pytest fixture to work, the function names need to being with text_
     """
     A sample dummy test for illustration. This test will always succeed.
     """
-    assert 2 == 2, "Two and two must be the same"
+    oldstdout = sys.stdout, "Two and two must be the same"
+    sys.stdout = capture
+    print("MEOW")
+    sys.stdout = oldstdout
 
-def test_dummy_fails():
+    capture.seek(0)
+    result = capture.read()
+    assert result == "MEOW\n", f"result (len = {len(result)}) = {result}, capture = {capture}"
+
+
+#def test_dummy_fails():
     """
-    A sample dummy test for illustration. This test will always fail.
+    A sample dummy test for illustration. This test will all fail.
     """
-    assert 2 == 3, "Two and three must be the same. (Really?)"
-
-
-
+#    assert 2 == 3, "Two and three must be the same. (Really?)"
