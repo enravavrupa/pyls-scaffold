702d9efef33fff9aa00bf1504cc553ceed764a33
Apurva Varne <apurvavarne@gmail.com>
2025-07-30 16:48:32 +0000

    pyls code updated to working code

--- pyls.py
+++ pyls.py
@@ -5,16 +5,13 @@ import os
 
 
 def main() -> None:
-    parser = argparse.ArgumentParser(
-        prog="pyls", description="A baby version of ls.")
+    parser = argparse.ArgumentParser(prog="pyls", description="A baby version of ls.")
 
     # We now add descriptions for the expected arguments for our program.
     parser.add_argument(
         "dirname",
-        # Indicates that either 0 or one directory name can be given.
-        nargs="?",
-        # Gives the default value to use when this argument is not given.
-        default=".",
+        nargs="?",  # Indicates that either 0 or one directory name can be given.
+        default=".",  # Gives the default value to use when this argument is not given.
         # '.' means "current directory".
         help="The name of the directory whose contents are to be listed.",
     )
@@ -45,9 +42,9 @@ def main() -> None:
     # flag, it will print out all of the specification above in a nice format
     # as help. This is a convention employed by most command line programs.
     args = parser.parse_args()
-    os.chdir("/home/avrupaenrav/")
-#    print(os.listdir("/home/avrupaenrav/"))
 
+#print(os.listdir("/home/avrupaenrav"))
+os.chdir("/home")
 
 def pyls(dirname: str, longform: bool, formatted: bool) -> None:
     """
@@ -57,8 +54,8 @@ def pyls(dirname: str, longform: bool, formatted: bool) -> None:
     In this case, we're choosing a **representation** where we represent
     the directory name to list as a string and the choice of longform and
     formatted output as two boolean values.
-
-    SIGNATURE
+   
+    SIGNATURE 
     ---------
     The "signature" of the procedure below can be written as
               str bool bool -> None
@@ -66,11 +63,11 @@ def pyls(dirname: str, longform: bool, formatted: bool) -> None:
     PURPOSE
     -------
 
-    - :param dirname: asks you for the name of the directory whose information
-                      you want
-    - :param longform: if true, then it lists all relevant data in longform
-    - :param formatted: if true, then it uses '\' to indicate if a file is a
-            directory
+    TODO: Replace this content with a sentence or two describing what this
+    function does.
+    - :param dirname: TODO: Description of `dirname` parameter.
+    - :param longform: TODO: Description of `longform` parameter.
+    - :param formatted: TODO: Description of `formatted` parameter.
 
     EXAMPLES
     --------
@@ -78,32 +75,28 @@ def pyls(dirname: str, longform: bool, formatted: bool) -> None:
     TODO: Below, give a few examples of what you expect the procedure to do when you
     give various inputs. This can help you think about what to implement.
     Consider various possible combinations.
-
-    def(abc, True, True) -> returns elements of 'abc' in longform and formatted
-
     """
-    for i in range(os.listdir(dirname)):
-        if os.path.isfile(i):
-            if longform and formatted:
-                print(i, os.path.getmtime(i), os.path.getsize())
-            elif longform:
-                print(i, os.path.getmtime(i), os.path.getsize())
-            elif formatted:
-                print(i, os.path.getmtime(i), os.path.getsize())
+    # Replace the "pass" below with your implementation.
+    dirpath = os.path.join("/home", dirname)
+    lst = os.listdir(dirpath)
+    for i in lst:
+        j = os.path.join(dirpath, i)
+        if longform and formatted:
+            if os.path.isfile(j):
+                print(f"{os.path.getmtime(j)} | {os.path.getsize(j)} |  {os.path.basename(j)}")
             else:
-                print(i)
+                print(f"{os.path.getmtime(j)} | {os.path.getsize(j)} |  {os.path.basename(j)}/")
+        elif longform:
+            print(f"{os.path.getmtime(j)} | {os.path.getsize(j)} |  {os.path.basename(j)}")
+        elif formatted:
+            if os.path.isfile(j):
+                print(f"{os.path.basename(j)}")
+            else:
+                print(f"{os.path.basename(j)}/")
         else:
-            if longform and formatted:
-                print(str(i)+ "/", os.path.getmtime(i), os.path.getsize())
-            elif longform:
-                print(i, os.path.getmtime(i), os.path.getsize())
-            elif formatted:
-                print(str(i)+ "/", os.path.getmtime(i), os.path.getsize())
-            else:
-                print(i)
-# use os.path.isfile(path) to check if an object is file and then if it is
-# a file then run pyls on it
+            print("{os.path.basename(j)}") # this works
 
+pyls("avrupaenrav", True, True)
 # A python module may be loaded in one of two ways --
 # 1. `python myfile.py`: In this case, the python file/module is considered to be
 #    a "main program" and is named "__main__" within. So if you want to run code
@@ -111,6 +104,5 @@ def pyls(dirname: str, longform: bool, formatted: bool) -> None:
 # 2. `import myfile`: (within some other python file). In this mode, it is a pure
 #    "module" that exposes functions and values via `myfile.` notation within the
 #    importing python file. In this case, __name__ will be "myfile" and not "__main__".
- 
 if __name__ == "__main__":
     main()
